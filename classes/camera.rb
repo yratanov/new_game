3728be632ac401404a6e79307bb8a6378049ae1b
yratanov <organium@gmail.com>
2013-09-18 22:00:38 +0800

    hope it looks more readable now.

--- classes/camera.rb
+++ classes/camera.rb
@@ -8,20 +8,38 @@ class Camera
   def target(level_object)
     @target = level_object
 
-    if @target.x + window.width/2 < window.level.width and @target.x - window.width/2 > 0
+    if not right_border_intersect? and not left_border_intersect?
       @x = @target.x - window.width/2
-    elsif @target.x - window.width/2 < 0
+    elsif left_border_intersect?
       @x = 0
     else
       @x = window.level.width - window.width
     end
 
-    if @target.y + window.height/2 < window.level.height and @target.y - window.height/2 > 0
+    if not top_border_intersect? and not bottom_border_intersect?
       @y = @target.y - window.height/2
-    elsif @target.y - window.height/2 < 0
+    elsif top_border_intersect?
       @y = 0
     else
       @y = window.level.height - window.height
     end
+  end
+
+  private
+
+  def right_border_intersect?
+    @target.x + window.width/2 > window.level.width
+  end
+
+  def left_border_intersect?
+    @target.x - window.width/2 < 0
+  end
+
+  def top_border_intersect?
+    @target.y - window.height/2 < 0
+  end
+
+  def bottom_border_intersect?
+    @target.y + window.height/2 > window.level.height
   end
 end
