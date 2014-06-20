ab29d3f25802c3f6941f292a50f2df26348c3d33
varg90 <kuhn.alexey@gmail.com>
2014-06-20 15:35:26 +0800

    bombs added. explosion not animated yet

---
+++ lib/level_object/bomb.rb
@@ -0,0 +1,55 @@
+require 'level_object/touch_strategy/base'
+
+module LevelObject
+  class Bomb < LevelObject::Base
+
+    def image
+      if exploded?
+        @images = Animation.new(explode_images)
+      else
+        @image = @image_registry.image('bomb/bomb.png')
+      end
+    end
+
+    def explode_images
+      %w(explosion_1 explosion_2 explosion_3 explosion_4 empty).map do |file|
+        @image_registry.image("bomb/#{file}.png")
+      end
+    end
+
+    def touch_right(object)
+      explode(object)
+    end
+
+    def touch_left(object)
+      self.touch_right(object)
+    end
+
+    def touch_top(object)
+      self.touch_right(object)
+    end
+
+    def touch_bottom(object)
+      self.touch_right(object)
+    end
+
+    def explode(object)
+      if not exploded?
+        # object.damage(1)
+      end
+      @exploded = true
+    end
+
+    def exploded?
+      @exploded
+    end
+
+    def touch_strategy
+      @touch_strategy ||= TouchStrategy::Base.new(self)
+    end
+
+    def touch_strategy=(strategy)
+      @touch_strategy = strategy
+    end
+  end
+end
