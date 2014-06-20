5d06a3990a60dc7c55ef4806c5717f53fa27b6ce
yratanov <organium@gmail.com>
2014-06-20 17:21:51 +0800

    spec fix, travis yml update

--- spec/animation_spec.rb
+++ spec/animation_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'animation'
 
 describe Animation do
 
--- spec/camera_spec.rb
+++ spec/camera_spec.rb
@@ -6,30 +6,30 @@ describe Camera do
 
   let(:level) { double(width: 150, height: 150) }
   let(:window) { double(level: level, width: 20, height: 20) }
-  subject {Camera.new(window)}
+  subject { Camera.new(window) }
 
   context '#target' do
     it 'should center camera' do
       object = double(x: 50, y: 50)
       subject.target(object)
 
-      subject.x.should == 40
-      subject.y.should == 40
+      subject.rectangle.x.should == 40
+      subject.rectangle.y.should == 40
     end
 
     it 'should not show level end' do
       object = double(x: 150, y: 150)
       subject.target(object)
 
-      subject.x.should == 130
-      subject.y.should == 130
+      subject.rectangle.x.should == 130
+      subject.rectangle.y.should == 130
 
 
       object = double(x: 2, y: 3)
       subject.target(object)
 
-      subject.x.should == 0
-      subject.y.should == 0
+      subject.rectangle.x.should == 0
+      subject.rectangle.y.should == 0
     end
   end
 
