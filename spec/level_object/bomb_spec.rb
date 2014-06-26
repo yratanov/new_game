9df60c7e17bfc224bf6ac99826d9d5e230fedaf5
varg90 <kuhn.alexey@gmail.com>
2014-06-27 04:34:45 +0800

    fix spec

--- spec/level_object/bomb_spec.rb
+++ spec/level_object/bomb_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'level_object/bomb'
+require 'config'
 
 describe LevelObject::Bomb do
   subject { described_class.new(image_registry, x, y) }
@@ -8,9 +9,20 @@ describe LevelObject::Bomb do
   let(:y) { 20 }
 
   context '#touch' do
-    it 'should call mark_to_destroy' do
+    let(:player) { double }
+    let(:bomb_config) { [damage: '1'] }
+    let(:object) { double(get_damage: double) }
+    let(:get_damage) { double }
+
+    before do
+      Game::Config.stub(:load).and_return(bomb_config[0])
+      subject.stub(:object).and_return(object)
+    end
+
+    it 'should call get_damage and mark_to_destroy' do
+      expect(object).to receive(:get_damage)
       expect(subject).to receive(:mark_to_destroy)
-      subject.touch
+      subject.touch(object)
     end
   end
 end
